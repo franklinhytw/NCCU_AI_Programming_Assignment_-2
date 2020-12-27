fc031e2a98c96d36189aa43152b2c03a1fec6154
franklin <franklinhytw@gmail.com>
2020-12-28 00:24:36 +0800

    Update

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,22 +4,25 @@
 #include <math.h>
 #include <algorithm>
 
-#define SQR               vector<vector<uint8_t>>
+#define SQR               vector<ST_BOARD>
 #define QUUEN_LIST_STRUCT vector<tuple<uint8_t, uint8_t>>
 
 using namespace std;
 
-void printBoard(SQR &board) {
+typedef vector<uint16_t> ST_BOARD;
+
+void printBoard(ST_BOARD &board) {
     size_t count = board.size();
-    for (size_t x = 0 ; x < count ; x++) {
-        for (size_t y = 0 ; y < count ; y++) {
-            if(board[x][y] == 1) {
+    for (size_t x = 0; x < count; x++) {
+        for (size_t y = 0; y < count; y++) {
+            if (board[y] == x+1) {
                 printf("Q|");
-            }
-            else printf(" |");
+            } else
+                printf(" |");
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 #if 0
@@ -42,7 +45,7 @@ QUUEN_LIST_STRUCT availableNextStep(SQR &board) {
 }
 #endif
 
-bool isAttcked(vector<uint16_t> &board) {
+bool isAttcked(ST_BOARD &board) {
     for (size_t i = 0 ; i < board.size() ; i++) {
         auto &s = board.at(i);
         if(s) {
@@ -56,23 +59,30 @@ bool isAttcked(vector<uint16_t> &board) {
     }
 }
 
-void putQueen(vector<uint16_t> &board) {
-    vector<uint8_t> queen_index; // start from zero
+void putQueen(ST_BOARD &board) {
+    uint16_t queen_index = 0;
+    // ST_BOARD queen_index; // start from zero
 
     // find the quuen
     for (size_t i = 0 ; i < board.size() ; i++) {
         if(board.at(i) > 0) {
-            queen_index.push_back(i);
+            queen_index = i;
+            break;
+            // queen_index.push_back(i);
         }
+    }
+
+    for (size_t i = queen_index+1 ; i < board.size() ; i++) {
+        
     }
 
     for (size_t i = 0 ; i < board.size() ; i++) {
         auto it = find(queen_index.begin(), queen_index.end(), i);
         if (it != queen_index.end()) continue;
 
-        vector<uint16_t> unavailable_state; 
+        ST_BOARD unavailable_state; 
         for (const auto &qi : queen_index) {
-            uint16_t cur_num = (uint16_t)(1 << qi-1);
+            uint8_t cur_num = (uint8_t)(1 << qi-1);
         }
     }
     // for (size_t q = 0 ; q < queen_index.size()-1 ; q++) {
@@ -82,12 +92,12 @@ void putQueen(vector<uint16_t> &board) {
     // }
 }
 
-void solve(vector<uint16_t> &arg_init_queen) {
+void solve(ST_BOARD &arg_init_queen) {
     // transfer to binary mode
-    vector<uint16_t> board;
+    ST_BOARD board;
 
     for (const auto &pos : arg_init_queen) {
-        board.push_back((uint16_t)(1 << pos-1));
+        board.push_back((uint8_t)(1 << pos-1));
     }
 
     for (const auto &pos : board) {
@@ -95,13 +105,14 @@ void solve(vector<uint16_t> &arg_init_queen) {
     }
     cout << endl;
 
-    putQueen(board);
+    // putQueen(board);
 }
 
 int main(int argc, char const *argv[]) {
-    uint8_t n = 8;
+    // uint8_t n = 8;
     
-    vector<uint16_t> init_queen {1,3,0,0,0,0,0,0};
+    ST_BOARD init_queen {3,0,0,0,0,0,0,0};
+    printBoard(init_queen);
     solve(init_queen);
     
     // 1 2 4 8 16 32 64 128 
