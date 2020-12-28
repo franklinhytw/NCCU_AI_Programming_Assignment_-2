5b09b042656ea033b4b36e6af077e6955bb0189d
franklin <franklinhytw@gmail.com>
2020-12-29 01:02:25 +0800

    almost done!

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <tuple>
 #include <math.h>
-#include <algorithm>
-#include <set>
 #include <cstdlib>
+#include <string>
+#include <cstring>
+#include <random>
 
 #define QUUEN_LIST_STRUCT vector<tuple<uint8_t, uint8_t>>
 
@@ -27,41 +27,8 @@ void printBoard(ST_BOARD &board) {
     printf("\n");
 }
 
-#if 0
-QUUEN_LIST_STRUCT availableNextStep(SQR &board) {
-    size_t count = board.size();
-    QUUEN_LIST_STRUCT queen_list;
-
-    // find all queen position
-    for (size_t x = 0 ; x < count ; x++) {
-        for (size_t y = 0 ; y < count ; y++) {
-            if(board[x][y] == 1) {
-                tuple<uint8_t, uint8_t> pos(x,y);
-                queen_list.push_back(pos);
-            }
-        }
-    }
-
-
-    return true;
-}
-#endif
-
-bool isAttcked(ST_BOARD &board) {
-    for (size_t i = 0 ; i < board.size() ; i++) {
-        auto &s = board.at(i);
-        if(s) {
-            auto it = find(board.begin(), board.end(), s);
-            if (it != board.end()) return true;
-            else {
-                int start_dis = i-0;
-            }
-        }
-    }
-}
-
 bool solve(ST_BOARD arg_init_queen, int8_t index, uint8_t queen_pos) {
-    if(index >= 0) arg_init_queen.at((int)index) = queen_pos;
+    if(index >= 0) arg_init_queen.at(index) = queen_pos;
     // (index, queen_pos)
     unordered_map<uint8_t, uint8_t> q_mapping;
 
@@ -75,10 +42,11 @@ bool solve(ST_BOARD arg_init_queen, int8_t index, uint8_t queen_pos) {
     uint8_t n = arg_init_queen.size();
 
     if(q_mapping.size() == n) {
+        cout << "====== GOAL STATE =======" << endl;
         printBoard(arg_init_queen);
+        cout << "=========================" << endl;
         return true;
     }
-
     // solving problem
     for (size_t t = 0 ; t < n ; t++) {
         auto it = q_mapping.find(t);
@@ -99,38 +67,99 @@ bool solve(ST_BOARD arg_init_queen, int8_t index, uint8_t queen_pos) {
                 if(is_attacked) continue;
                 else {
                     //# put the queen
-                    if(solve(arg_init_queen, (int)t, num)) {
+                    if(solve(arg_init_queen, (int8_t)t, num)) {
                         return true;
                     }
                 }
             }
+            // if no select any position, should be return previous index to select another position of queen
+            return false;
         }
     }
     return false;
 }
 
+vector<string> split(const string& str) {
+	vector<string> res;
+	if("" == str) return res;
+
+	char * strs = new char[str.length() + 1] ;
+	strcpy(strs, str.c_str()); 
+  
+	char *p = strtok(strs, ",");
+	while(p) {
+		string s = p;
+		res.push_back(s);
+		p = strtok(NULL, ",");
+	}
+ 
+	return res;
+}
+
+int randomInteger(int min, int max) {
+    random_device rd;
+	mt19937_64 generator = mt19937_64(rd());
+	uniform_int_distribution<int> distribution(min, max);
+
+	return distribution(generator);
+}
+
+void problem(ST_BOARD &board) {
+    cout << "===== INITIAL STATE =====" << endl;
+    printBoard(board);
+    cout << "=========================" << endl;
+
+    bool rs = solve(board, -1, 0);
+    if (rs) {
+        cout << "[V] Find the solution!" << endl;
+    } else
+        cout << "[X] No Answer!" << endl;
+}
+
 int main(int argc, char const *argv[]) {
+    if (argc >= 1) {
+		if(argc <= 2) {
+			cout << "Usage: [MODE]" << endl;
+        	cout << "  -random ${n}          n = >4 number" << endl;
+			cout << "  -init_state ${state}  $state example: '0,0,0,0,0,0,3,0'" << endl;
+		}
+		else if(argc == 3) {
+			if(!strcmp(argv[1], "-random")) {
+				int n = atoi(argv[2]);
+
+                int index_rand = randomInteger(0, n-1);
+                int queen_pos_rand = randomInteger(1, n);
+
+                ST_BOARD init_board;
+
+				for(size_t t = 0 ; t < n ; t++) {
+                    if (t == (size_t)index_rand) init_board.push_back((uint8_t)queen_pos_rand);
+                    else init_board.push_back(0);
+                }
+
+                problem(init_board);
+			}
+			else if(!strcmp(argv[1], "-init_state")) {
+				string init_state = argv[2];
+                vector<string> n_list = split(init_state);
+                ST_BOARD init_board;
+                for(const auto &n : n_list) {
+                    init_board.push_back((uint8_t)stoi(n));
+                }
+
+                problem(init_board);
+			}
+		}
+		exit(EXIT_SUCCESS);
+    }
+    
     // uint8_t n = 8;
     
-    ST_BOARD init_queen {3,0,0,0,0,0,0,0};
-    printBoard(init_queen);
-    bool ans = solve(init_queen, -1, 0);
-    
-    // 1 2 4 8 16 32 64 128 
-    
-    // SQR init_state {
-    //     {0, 0, 0, 0, 0, 0, 0, 0},
-    //     {0, 0, 0, 0, 0, 0, 0, 0},
-    //     {Q, 0, 0, 0, 0, 0, 0, 0},
-    //     {0, 0, 0, 0, 0, 0, 0, 0},
-    //     {0, 0, 0, 0, 0, 0, 0, 0},
-    //     {0, 0, 0, 0, 0, 0, 0, 0},
-    //     {0, 0, 0, 0, 0, 0, 0, 0},
-    //     {0, 0, 0, 0, 0, 0, 0, 0}
-    // };
-
-    // cout << "Initial State:" << endl;
-    // printBoard(init_state);
+    // ST_BOARD init_queen {3,0,0,0,0,0,0,0};
+    // ST_BOARD init_queen {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    // printBoard(init_queen);
+    // bool ans = solve(init_queen, -1, 0);
+    // cout << ans << endl;
 
     return 0;
 }
